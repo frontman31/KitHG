9757c5a68189c28c9c298b2f0006aafc87d94233
frontman31 <htcflo90@googlemail.com>
2014-07-11 19:59:10 +0200

    1. Commit

---
+++ src/net/bernielDE/kithg/main/KHGMainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.bernielDE.kithg.main;
+
+/**
+ * @author frontman31
+ * @version 0.1
+ * 
+ * @creation 11.07.2014 at 19:58:02
+ * @class KHGMainClass.java in @package net.bernielDE.kithg.main in @project KitHG
+ **/
+
+public class KHGMainClass {
+	
+	private int i = 1;
+	
+	public int getI() {
+		return i;
+	}
+}
